faea1ec89ef383aec1cf8b9b1300178dcdb1c5a2
Nemo Xiong <xiongnemo@126.com>
2021-02-09 18:29:07 +0800

    gitalk: POST -> GET

--- dist/gitalk.js
+++ dist/gitalk.js
@@ -6829,7 +6829,7 @@ var GitalkComponent = function (_Component) {
         id: replacedUrl
       }, props.options);
 
-      _util.axiosJSON.post(_this.options.proxy + code).then(function (res) {
+      _util.axiosJSON.get(_this.options.proxy + code).then(function (res) {
         if (res.data && res.data.access_token) {
           _this.accessToken = res.data.access_token;
 
